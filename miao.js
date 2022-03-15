a56954f17e859e20770aa40c02f5b22473d7e241
SomeSomewhere <cygnusborjigin@gmail.com>
2022-03-15 14:49:10 -0400

    ordered data

--- miao.js
+++ miao.js
@@ -13,11 +13,11 @@ const cleanPrice = (rawString) => {
 	return rawString.substring(0, index);
 }
 
-const getRawData = async () => {
+const getRawData = async (target) => {
 	try {
 		var config = {
 			method: 'get',
-  			url: 'https://www.google.com/search?q=google+stock+price',
+  			url: `https://www.google.com/search?q=${target}+stock+price`,
   			headers: { 
     				'Cookie': '1P_JAR=2022-03-15-17; NID=511=mct2HN5CGR6tYa0Sl68bcCOkLgK8zN_D34FByVD3ZvZsmoejcDMLnbkwndsLmzQJiQiXWgTU-Sw1OcFVW8jMkexZ5oFIEsn6PV83Q0iYwBXfCKn7VRQqAsMrhaC8Sj0CsiGwyg-iTkR8z8YUPgYD5S1P-W5PYf4k78_utHNosFI'
   			}
@@ -29,10 +29,15 @@ const getRawData = async () => {
 		// get price
 		const index = getPosition(rawData, "BNeawe iBp4i AP7Wnd", 2);
 		const price = rawData.substring(index+21, index+31);
-		console.log(price);
-		console.log(cleanPrice(price));
-		
-		// clean price
+		const cleaned = cleanPrice(price);
+
+		// order data
+		const dateObj = new Date();
+		console.log(`Date: ${dateObj.toDateString()}`);
+		console.log(`Time: ${dateObj.toTimeString()}`);
+		const inOrder = [`${target}`, `${dateObj.toDateString()}`, `${dateObj.toTimeString()}`, `${cleaned}`];
+		console.log(inOrder);
+
 
 	} catch (err) {
 		console.log(err);
@@ -40,5 +45,4 @@ const getRawData = async () => {
 
 };
 
-getRawData();
-
+target.forEach(each => getRawData(each));
